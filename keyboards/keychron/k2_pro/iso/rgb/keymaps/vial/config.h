dd5af71e700056f582681a7779c14798f70494b5
bfayers <bfayers@users.noreply.github.com>
2023-12-12 15:50:48 +0000

    esc+enter as security combo

--- keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
@@ -5,5 +5,5 @@
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0x51, 0xD8, 0x56, 0x36, 0x03, 0x90, 0x26, 0xF5}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
