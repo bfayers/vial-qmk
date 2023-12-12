97cfb443281988ed76bad0833cabf186e27aeab5
bfayers <bfayers@users.noreply.github.com>
2023-12-12 16:28:43 +0000

    security lock/unlock is esc+ent now

--- keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
@@ -6,4 +6,4 @@
 #define VIAL_KEYBOARD_UID {0x51, 0xD8, 0x56, 0x36, 0x03, 0x90, 0x26, 0xF5}
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
