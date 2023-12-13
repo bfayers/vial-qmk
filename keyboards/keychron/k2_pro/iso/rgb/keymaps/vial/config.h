767fe45fcc3f6de16626aa6f039f0e6e706a82bc
bfayers <bfayers@users.noreply.github.com>
2023-12-13 22:58:44 +0000

    personal keymap stuff

--- keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
@@ -5,7 +5,9 @@
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0x51, 0xD8, 0x56, 0x36, 0x03, 0x90, 0x26, 0xF5}
 
+//Set VIAL Secure unlock to ESC+ENTER
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
 
+//Set default RGB mode to 'Splash'
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SPLASH
